00cdbd7556b97e5116f6386220f547803cd19549
Chris Wheeler <grintor@gmail.com>
2017-12-26 12:45:54 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-wget http://telily.com/downloads/gs-afterhours/gs-afterhours.sh -O /opt/gs-afterhours.sh
-wget http://telily.com/downloads/gs-afterhours/gs-cfg2bin.pl -O /opt/gs-cfg2bin.pl
+wget http://telily.com/downloads/gs-afterhours/gs-afterhours.txt -O /opt/gs-afterhours.sh
+wget http://telily.com/downloads/gs-afterhours/gs-cfg2bin.txt -O /opt/gs-cfg2bin.pl
 
-echo "0 20 * * * cd /opt/ && ./gs-afterhours.sh on" > /etc/cron.d/gs-afterhours-on
-echo "0 6 * * * cd /opt/ && ./gs-afterhours.sh off" > /etc/cron.d/gs-afterhours-off
+echo "0 20 * * * root cd /opt/ && ./gs-afterhours.sh on" > /etc/cron.d/gs-afterhours-on
+echo "0 6 * * * root cd /opt/ && ./gs-afterhours.sh off" > /etc/cron.d/gs-afterhours-off
 
 chmod 0644 /etc/cron.d/gs-afterhours-off
 chmod 0644 /etc/cron.d/gs-afterhours-on
