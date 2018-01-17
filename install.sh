a8d0efcd3c580bdb86f101587909539d24f661f5
Chris Wheeler <grintor@gmail.com>
2018-01-17 16:28:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,16 +6,19 @@ wget http://telily.com/downloads/gs-afterhours/gs-afterhours.txt -O /opt/gs-afte
 wget http://telily.com/downloads/gs-afterhours/gs-cfg2bin.txt -O /opt/gs-cfg2bin.pl
 
 echo "SHELL=/bin/bash" > /etc/cron.d/gs-afterhours-on
+echo "HOME=/opt" >> /etc/cron.d/gs-afterhours-on
 echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" >> /etc/cron.d/gs-afterhours-on
-echo "45 19 * * * root cd /opt/ && ./gs-afterhours.sh on && ./gs-afterhours.sh reboot" >> /etc/cron.d/gs-afterhours-on
+echo "45 19 * * * root ./gs-afterhours.sh on && ./gs-afterhours.sh reboot" >> /etc/cron.d/gs-afterhours-on
 
 echo "SHELL=/bin/bash" > /etc/cron.d/gs-afterhours-off
+echo "HOME=/opt" >> /etc/cron.d/gs-afterhours-off
 echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" >> /etc/cron.d/gs-afterhours-off
-echo "30 6 * * * root cd /opt/ && ./gs-afterhours.sh off && ./gs-afterhours.sh reboot" >> /etc/cron.d/gs-afterhours-off
+echo "30 6 * * * root ./gs-afterhours.sh off && ./gs-afterhours.sh reboot" >> /etc/cron.d/gs-afterhours-off
 
 echo "SHELL=/bin/bash" > /etc/cron.d/gs-afterhours-reboot
+echo "HOME=/opt" >> /etc/cron.d/gs-afterhours-reboot
 echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" >> /etc/cron.d/gs-afterhours-reboot
-echo "45 6 * * * root cd /opt/ && ./gs-afterhours.sh reboot" >> /etc/cron.d/gs-afterhours-reboot
+echo "45 6 * * * root ./gs-afterhours.sh reboot" >> /etc/cron.d/gs-afterhours-reboot
 
 chmod 0644 /etc/cron.d/gs-afterhours-off
 chmod 0644 /etc/cron.d/gs-afterhours-on
